072d05cbe7ae814d646dff15b0241b28cfcd6298
Lucas Rogério Masotti <lucasmasotti@gmail.com>
2023-11-15 13:38:13 -0300

    First commit

---
+++ src/main/java/com/event/management/EventManagementApplication.java
@@ -0,0 +1,13 @@
+package com.event.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/event/management/EventManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.event.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

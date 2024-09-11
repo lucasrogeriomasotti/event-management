0c2437978432f89dc86b00d472e3d19e76629d97
Lucas Rogério Masotti <lucasmasotti@gmail.com>
2024-09-10 23:48:41 -0300

    First commit

---
+++ src/test/java/br/com/luroma/event/management/EventManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luroma.event.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
